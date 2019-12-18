3c381b7b3bbbf202e5c57af15164c99ec0bc47c4
Brandon Lee <brandon@lee.com>
2019-12-18 18:02:26 +0900

    https://jusths.tistory.com/69

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world!")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8888"
+	}
+	log.Printf("[MAIN] Server is running at 0.0.0.0:%v\n", port)
+	log.Println("[MAIN]", e.Start(":"+port))
+}
